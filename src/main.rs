84b88ae1c7a575a97b430739742988c94f735c9a
likanug <likanug.g@qq.com>
2025-06-07 12:14:50 +0800

    添加简单的测试用例

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use std::collections::HashMap;
+
+use mini_redis::{Command, Connection, Frame};
+use tokio::net::{TcpListener, TcpStream};
+
+async fn process(socket: TcpStream) {
+    let mut db = HashMap::new();
+
+    let mut connection = Connection::new(socket);
+
+    while let Some(frame) = connection.read_frame().await.unwrap(){
+        let response = match Command::from_frame(frame).unwrap() {
+            Command::Set(cmd) => {
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            }
+            Command::Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone().into())
+                }else {
+                    Frame::Null
+                }
+            }
+            cmd=> panic!("unimplemented {:?}", cmd),
+        };
+
+        connection.write_frame(&response).await.unwrap();
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    // Bind the listener to the address
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    loop {
+        // The second item contains the ip and port of the new connection.
+        let (socket, _) = listener.accept().await.unwrap();
+
+        // A new task is spawned for each inbound socket.  The socket is
+        // moved to the new task and processed there.
+        tokio::spawn(async move {
+            process(socket).await;
+        });
+    }
+}
