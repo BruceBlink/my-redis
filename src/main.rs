c25a93bfc8228d926a8dba72d0c92c39cab6ab39
likanug <likanug.g@qq.com>
2025-06-07 20:41:42 +0800

    添加线程安全的共享hashmap,使每一个线程共同操作这个map

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use bytes::Bytes;
 
 use mini_redis::{Command, Connection, Frame};
 use tokio::net::{TcpListener, TcpStream};
 
-async fn process(socket: TcpStream) {
-    let mut db = HashMap::new();
+//定义db的别名
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
+async fn process(socket: TcpStream, db: Db) {
     let mut connection = Connection::new(socket);
 
     while let Some(frame) = connection.read_frame().await.unwrap(){
@@ -33,14 +36,19 @@ async fn main() {
     // Bind the listener to the address
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
+    println!("Server is Listening....");
+    // 使用线程安全的共享hashmap
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
         // The second item contains the ip and port of the new connection.
         let (socket, _) = listener.accept().await.unwrap();
 
+        let db = db.clone();
         // A new task is spawned for each inbound socket.  The socket is
         // moved to the new task and processed there.
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
