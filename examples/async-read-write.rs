6ed70c17adf84bdfb1b46ed1a3cbb92d2770af49
likanug <likanug.g@qq.com>
2025-06-08 13:27:29 +0800

    使用Vec作为buffer读取文件

--- examples/async-read-write.rs
+++ examples/async-read-write.rs
@@ -4,11 +4,16 @@ use tokio::io::{self, AsyncReadExt};
 #[tokio::main]
 async fn main() -> io::Result<()> {
     let mut f = File::open("foo.txt").await?;
-    let mut buffer = [0; 10];
+/*    let mut buffer = [0; 10];
 
     // read up to 10 bytes
     let n = f.read(&mut buffer[..]).await?;
 
-    println!("The bytes: {:?}", &buffer[..n]);
+    println!("The bytes: {:?}", &buffer[..n]);*/
+
+    let mut buffer = Vec::new();
+    f.read_to_end(&mut buffer).await?;
+    println!("buffer: {:?}", buffer);
+
     Ok(())
 }
