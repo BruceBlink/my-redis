b9d5cf8c7b3846c7f44b21daa9e1def845f86c43
likanug <likanug.g@qq.com>
2025-06-08 12:09:43 +0800

    同步tokio任务

--- examples/mpsc-channel.rs
+++ examples/mpsc-channel.rs
@@ -1,19 +1,59 @@
+use bytes::Bytes;
+use mini_redis::client;
 use tokio::sync::mpsc;
+
+use crate::Command::{Get, Set};
+
+#[derive(Debug)]
+enum Command {
+    Get {
+        key: String,
+    },
+    Set {
+        key: String,
+        val: Bytes,
+    }
+}
 
 #[tokio::main]
 async fn main() {
+    // 创建channel
     let (tx, mut rx) = mpsc::channel(32);
+
+    let manager = tokio::spawn(async move{
+        let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+
+        while let Some(cmd) = rx.recv().await {
+            match cmd {
+                Get {key} => {
+                    client.get(&key).await.expect("got value error! ");
+                }
+                Set {key,val} => {
+                    client.set(&key, val).await.expect("set value error");
+                }
+            }
+
+        }
+    });
+
     let tx2 = tx.clone();
 
-    tokio::spawn(async move {
-        tx.send("sending from first handle").await.unwrap();
+    let t1 = tokio::spawn(async move {
+        let cmd = Command::Get {
+            key: "foo".to_string(),
+        };
+        tx.send(cmd).await.unwrap();
     });
 
-    tokio::spawn(async move {
-        tx2.send("sending from second handle").await.unwrap();
+    let t2 = tokio::spawn(async move{
+        let cmd = Command::Set {
+            key: "foo".to_string(),
+            val: "bar".into(),
+        };
+        tx2.send(cmd).await.unwrap();
     });
 
-    while let Some(message) = rx.recv().await {
-        println!("GOT = {}", message);
-    }
+    t1.await.unwrap();
+    t2.await.unwrap();
+    manager.await.unwrap();
 }
