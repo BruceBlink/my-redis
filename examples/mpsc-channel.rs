fd81c9f054ad4a648d3b96e7b189273429fd9589
likanug <likanug.g@qq.com>
2025-06-08 13:15:30 +0800

    获取响应

--- examples/mpsc-channel.rs
+++ examples/mpsc-channel.rs
@@ -1,17 +1,21 @@
 use bytes::Bytes;
 use mini_redis::client;
-use tokio::sync::mpsc;
+use tokio::sync::{mpsc, oneshot};
 
 use crate::Command::{Get, Set};
+
+type Responder<T> = oneshot::Sender<mini_redis::Result<T>>;
 
 #[derive(Debug)]
 enum Command {
     Get {
         key: String,
+        resp: Responder<Option<Bytes>>
     },
     Set {
         key: String,
         val: Bytes,
+        resp: Responder<()>
     }
 }
 
@@ -25,11 +29,15 @@ async fn main() {
 
         while let Some(cmd) = rx.recv().await {
             match cmd {
-                Get {key} => {
-                    client.get(&key).await.expect("got value error! ");
+                Get {key, resp} => {
+                    let res = client.get(&key).await;
+                    // 忽略异常
+                    let _ = resp.send(res);
                 }
-                Set {key,val} => {
-                    client.set(&key, val).await.expect("set value error");
+                Set {key,val, resp} => {
+                    let res = client.set(&key, val).await;
+                    // 忽略异常
+                    let _ = resp.send(res);
                 }
             }
 
@@ -39,18 +47,26 @@ async fn main() {
     let tx2 = tx.clone();
 
     let t1 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel();
         let cmd = Command::Get {
             key: "foo".to_string(),
+            resp: resp_tx,
         };
         tx.send(cmd).await.unwrap();
+        let res= resp_rx.await;
+        println!("GOT = {:?}", res)
     });
 
     let t2 = tokio::spawn(async move{
+        let (resp_tx, resp_rx) = oneshot::channel();
         let cmd = Command::Set {
             key: "foo".to_string(),
             val: "bar".into(),
+            resp: resp_tx,
         };
         tx2.send(cmd).await.unwrap();
+        let res = resp_rx.await;
+        println!("GOT = {:?}", res);
     });
 
     t1.await.unwrap();
